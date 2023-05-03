49c7128b8fc4ca386a7dd37089a0f6ac33b9aba0
Jeph <ericjeffrey.sch@gmail.com>
2023-05-03 17:39:34 +0800

    Basic Type: Objects

--- app.js
+++
@@ -1,7 +0,0 @@
-function add(n1, n2) {
-    return n1 + n2; //specify the core type of the parameters
-}
-var number1 = 5;
-var number2 = 2.9;
-var result = add(number1, number2);
-console.log(result);
---
+++ basic_types/objects.ts
@@ -0,0 +1,9 @@
+const person: {
+    name: string,
+    age: number  //key name and specifying the type of each key
+}= {
+    name: 'Jeff',
+    age: 22
+};
+
+console.log(person.name);
