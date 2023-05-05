1536b28a1f4014b687c9299cf5fb87629a3445bb
Jeph <ericjeffrey.sch@gmail.com>
2023-05-05 16:28:19 +0800

    Function Types

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-//return_type.ts
-function printResult(n1, n2) {
-    console.log('Result: ' + (n1 + n2));
-    //void return types mean that your function does not return anything
+//function_types.ts
+//Function Types are types that describe the parameters of a functions.
+//They are created with arrow function notations () => 
+function divide(n1, n2) {
+    var q = n1 / n2;
+    console.log(q);
+    return q;
 }
-function subtract(n1, n2) {
-    var diff = n1 - n2;
-    var result = '';
-    if (diff < 0) {
-        result = 'The difference is negative';
-    }
-    else {
-        result = 'The difference is zero or positive';
-    }
-    console.log(diff);
-    return result;
-}
-printResult(10, 20);
-console.log(subtract(20, 30));
+var getQuotient;
+//getQuotient is a type that can handle a function that returns a number
+getQuotient = divide;
+//the divide function fits perfectly for the new Function Type getQuotient
+console.log(getQuotient(20, 30));
