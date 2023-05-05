5c349e50f12aa57ca192a28b97fc05f73a2480ea
Jeph <ericjeffrey.sch@gmail.com>
2023-05-05 17:00:21 +0800

    .

--- app.ts
+++
@@ -1,9 +0,0 @@
-//never_type.ts
-//Never return types are implement if the function intends to throw an error
-//This can be also implemented on functions that will never return anything.
-
-function generateError(message: string, code: number): never {
-  throw { message: message, errorCode: code };
-}
-
-generateError("An error occurred!", 500);
